74878c5e328a63b579b3b3e0dbdf975505ca7291
Sohail <sohailnsu15@gmail.com>
2020-09-30 20:39:01 +0600

    init

---
+++ backend/data.js
@@ -0,0 +1,74 @@
+export default {
+    products:[{
+        _id: '1',
+        name: 'Slim Shirt',
+        Category: 'Shirt',
+        image: '/images/d1.jpg',
+        brand: 'Nike',
+        price: 60,
+        rating: 4.2,
+        numReviews: 10
+    }, 
+    {
+        _id: '2',
+        name: 'Polo Shirt',
+        Category: 'Shirt',
+        image: '/images/d1.jpg',
+        brand: 'Nike',
+        price: 60,
+        rating: 4.5,
+        numReviews: 10
+
+    },
+    {
+        _id: '3',
+        name: 'New Shirt',
+        Category: 'Shirt',
+        image: '/images/d1.jpg',
+        brand: 'Nike',
+        price: 40,
+        rating: 4.1,
+        numReviews: 10
+    },
+    {
+        _id: '4',
+        name: 'Slim Shirt',
+        Category: 'Shirt',
+        image: '/images/d1.jpg',
+        brand: 'Nike',
+        price: 50,
+        rating: 4.9,
+        numReviews: 14
+    },
+    {
+        _id: '5',
+        name: 'Slim Shirt',
+        Category: 'Shirt',
+        image: '/images/d1.jpg',
+        brand: 'Nike',
+        price: 10,
+        rating: 3.2,
+        numReviews: 60
+    },
+    {
+        _id: '6',
+        name: 'Slim Pant',
+        Category: 'Pant',
+        image: '/images/d1.jpg',
+        brand: 'Nike',
+        price: 60,
+        rating: 4.2,
+        numReviews: 10
+    },
+    {
+        _id: '7',
+        name: 'Slim Pant',
+        Category: 'Pant',
+        image: '/images/d1.jpg',
+        brand: 'Nike',
+        price: 60,
+        rating: 4.2,
+        numReviews: 10
+    }
+    ]
+}
